734b88b529634490d88a9b61056562bb5075c5bf
Aleksey <74228258+LeKSuS-04@users.noreply.github.com>
2021-05-15 17:05:02 +0300

    Delete ultimate.h

--- ultimate.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "./big_int/big_int.h"
-#include "./combinatorics/combinatorics.h"
-#include "./ns_int/ns_int.h"
-#include "./operations/operations.h"
-#include "./tools/tools.h"
